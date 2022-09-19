a1ff08aa4bfac1b19cc8170fec72ede7552849da
harshada <harshadasawant22@gmail.com>
2022-09-19 14:42:45 +0530

    First_commit

---
+++ src/main/java/tld/DisplayMessage.java
@@ -0,0 +1,15 @@
+package tld;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class DisplayMessage extends SimpleTagSupport {
+public void doTag() throws JspException, IOException 
+{ JspWriter out = getJspContext().getOut(); 
+	out.println("Good Morning!"); }
+
+}
+
